b98dc3502b4548c0ac4f20d7e9e0562d2f1c4933
raphael baptista <raphaelbaptista3@gmail.com>
2022-11-07 21:39:46 -0300

    tutorial https://spring.io/guides/gs/accessing-data-mysql/

---
+++ src/main/java/org/senai/prjjava/UsuarioController.java
@@ -0,0 +1,33 @@
+package org.senai.prjjava;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(path = "/api/usuario")
+@CrossOrigin("*")
+public class UsuarioController {
+
+    @Autowired
+    UsuarioRepository uRepository;
+
+    @PostMapping("/add")
+    public @ResponseBody String addUsuario(
+        @RequestParam String nome,
+        @RequestParam String email) {
+        
+      
+    }
+
+    @GetMapping("/all")
+    public @ResponseBody Iterable<Usuario> buscarUsuarios(){
+        return uRepository.findAll();
+    }
+    
+}
