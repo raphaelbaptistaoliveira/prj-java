0e51e644d3d4689729a9ed36b5811468677f9c8f
raphael baptista <raphaelbaptista3@gmail.com>
2022-11-11 00:32:11 -0300

    atualização

--- src/main/java/org/senai/prjjava/controller/UsuarioController.java
+++ src/main/java/org/senai/prjjava/controller/UsuarioController.java
@@ -1,10 +1,13 @@
-package org.senai.prjjava;
+package org.senai.prjjava.controller;
 
+import org.senai.prjjava.entity.Usuario;
+import org.senai.prjjava.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,21 +20,21 @@ public class UsuarioController {
     @Autowired
     UsuarioRepository uRepository;
     //http://localhost:8080/api/usuario/add?nome=raphaelbaptista3&email=raphaelbaptista3@gmail.com
-    @PostMapping("/add")
-    public @ResponseBody String addUsuario(
-        @RequestParam String nome,
-        @RequestParam String email) {
-        Usuario objU = new Usuario();
-        objU.setNome(nome);
-        objU.setEmail(email);
+    @PostMapping("/")
+    public @ResponseBody Integer addUsuario(@RequestBody Usuario objU) {
         uRepository.save(objU);
-        return "Salvo";
+        return objU.getId();
       
     }
 
-    @GetMapping("/all")
+    @GetMapping("/")
     public @ResponseBody Iterable<Usuario> buscarUsuarios(){
         return uRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public void buscarUsuarios(@PathParam Integer id){
+
     }
     
 }
