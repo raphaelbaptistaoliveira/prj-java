1cddc4fa7bdd19ea5d5456bc7b725884ae9a121e
raphael baptista <raphaelbaptista3@gmail.com>
2022-11-11 20:02:59 -0300

    atualização

--- src/main/java/org/senai/prjjava/controller/UsuarioController.java
+++ src/main/java/org/senai/prjjava/controller/UsuarioController.java
@@ -1,15 +1,19 @@
 package org.senai.prjjava.controller;
 
+import java.util.Optional;
+
 import org.senai.prjjava.entity.Usuario;
 import org.senai.prjjava.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -31,10 +35,24 @@ public class UsuarioController {
     public @ResponseBody Iterable<Usuario> buscarUsuarios(){
         return uRepository.findAll();
     }
-
+    //http://localhost:8080/api/usuario/2 - nesse exemplo é usado uma variável de path (caminho)
     @GetMapping("/{id}")
-    public void buscarUsuarios(@PathParam Integer id){
+    public @ResponseBody Optional<Usuario> buscarUsuario(@PathVariable Integer id){
+        return uRepository.findById(id);
 
+    }
+
+    @PutMapping("/")
+    public @ResponseBody Usuario atualizar(@RequestBody Usuario objU){
+        uRepository.save(objU);
+        return objU;
+
+    }
+
+    @DeleteMapping("/{id}")
+    public @ResponseBody String apagar(@PathVariable Integer id){
+        uRepository.deleteById(id);
+        return "Ok ao apagar!";
     }
     
 }
