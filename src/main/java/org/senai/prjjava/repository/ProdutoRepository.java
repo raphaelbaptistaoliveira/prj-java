5baea89a7fd4b39e5f8cef725290e2a5b00ab26c
raphael baptista <raphaelbaptista3@gmail.com>
2022-11-12 19:08:25 -0300

    Api backend 01

--- src/main/java/org/senai/prjjava/repository/ProdutoRepository.java
+++ src/main/java/org/senai/prjjava/repository/ProdutoRepository.java
@@ -3,6 +3,6 @@ package org.senai.prjjava.repository;
 import org.senai.prjjava.entity.Produto;
 import org.springframework.data.repository.CrudRepository;
 
-public class ProdutoRepository extends CrudRepository<Produto, Integer> {
+public interface ProdutoRepository extends CrudRepository<Produto, Integer> {
     
 }
