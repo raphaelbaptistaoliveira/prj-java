33ec3af42032ad8cad2a715dbc586cbb66d3d36f
raphael baptista <raphaelbaptista3@gmail.com>
2022-11-12 19:51:57 -0300

    atualizacao

--- src/main/java/org/senai/prjjava/controller/ProdutoController.java
+++ src/main/java/org/senai/prjjava/controller/ProdutoController.java
@@ -21,36 +21,36 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @CrossOrigin("*")
 public class ProdutoController {
     @Autowired
-    ProdutoRepository uRepository;
+    ProdutoRepository pRepository;
     //http://localhost:8080/api/produto/
     @PostMapping("/")
     public @ResponseBody Integer addProduto(@RequestBody Produto objU) {
-        uRepository.save(objU);
+        pRepository.save(objU);
         return objU.getId();
       
     }
 
     @GetMapping("/")
     public @ResponseBody Iterable<Produto> listarProdutos(){
-        return uRepository.findAll();
+        return pRepository.findAll();
     }
 
     @GetMapping("/{id}")
     public @ResponseBody Optional<Produto> buscarProduto(@PathVariable Integer id){
-        return uRepository.findById(id);
+        return pRepository.findById(id);
 
     }
 
     @PutMapping("/")
     public @ResponseBody Produto atualizar(@RequestBody Produto objU){
-        uRepository.save(objU);
+        pRepository.save(objU);
         return objU;
 
     }
 
     @DeleteMapping("/{id}")
     public @ResponseBody String apagar(@PathVariable Integer id){
-        uRepository.deleteById(id);
+        pRepository.deleteById(id);
         return "Produto apagado!";
     }
     
