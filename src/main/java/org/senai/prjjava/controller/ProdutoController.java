3d356d30eca579dab0bd3536442fe36c986a3c0e
raphael baptista <raphaelbaptista3@gmail.com>
2022-11-12 19:05:07 -0300

    Api backend 01

---
+++ src/main/java/org/senai/prjjava/controller/ProdutoController.java
@@ -0,0 +1,57 @@
+package org.senai.prjjava.controller;
+
+import java.util.Optional;
+
+import org.senai.prjjava.entity.Produto;
+import org.senai.prjjava.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(path = "/api/produto")
+@CrossOrigin("*")
+public class ProdutoController {
+    @Autowired
+    ProdutoRepository uRepository;
+    //http://localhost:8080/api/produto
+    @PostMapping("/")
+    public @ResponseBody Integer addProduto(@RequestBody Produto objU) {
+        uRepository.save(objU);
+        return objU.getId();
+      
+    }
+
+    @GetMapping("/")
+    public @ResponseBody Iterable<Produto> listarProdutos(){
+        return uRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public @ResponseBody Optional<Produto> buscarProduto(@PathVariable Integer id){
+        return uRepository.findById(id);
+
+    }
+
+    @PutMapping("/")
+    public @ResponseBody Produto atualizar(@RequestBody Produto objU){
+        uRepository.save(objU);
+        return objU;
+
+    }
+
+    @DeleteMapping("/{id}")
+    public @ResponseBody String apagar(@PathVariable Integer id){
+        uRepository.deleteById(id);
+        return "Ok ao apagar!";
+    }
+    
+}
---
+++ src/main/java/org/senai/prjjava/repository/ProdutoRepository.java
@@ -0,0 +1,8 @@
+package org.senai.prjjava.repository;
+
+import org.senai.prjjava.entity.Produto;
+import org.springframework.data.repository.CrudRepository;
+
+public class ProdutoRepository extends CrudRepository<Produto, Integer> {
+    
+}
